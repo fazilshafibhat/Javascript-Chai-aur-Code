4ff07469c4a5168c88082f13e6871ccd4d3b17c7
FAZIL SHAFI <fazilshafi.fsb382@gmail.com>
2024-04-09 10:12:47 +0530

    01_basics completed

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 123456789;
+
+let accountEmail = "fazil@amazon.com";
+
+var accountPassword = "123@abc";
+
+accountCity = "Kashmir";
+
+let accountState;
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+/*
+    Prefer no to use var bcz of issue in
+    block scope and functional scope...
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict"; //Treat all JS code as new version
+console.log(3 + 
+    3)
+    ; //code readability should be high
+
+    let name = "Fazil";
+    let age = 13;
+    let isLoggedIn = false;
+    //-----------------Primitive types ----------------
+    // string number boolean null
+    // null is a standalone value -representation of empty value
+    // undefined: value not assigned
+    // symbol - used for unique values
+
+    //------------- Object types ----------------
